3a2681bcd351b322cb01c740bf815908f52cc8fd
Martins Ake <akemartins13@gmail.com>
2026-07-08 13:33:26 +0100

    update admin build failure

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="fixed inset-0 z-[100] bg-[#080f1e] overflow-y-auto">
